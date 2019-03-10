3024d385502b649c989f0002e0a2f0ab3759b21d
agong980814 <agong980814@gmail.com>
2019-03-10 12:24:30 +0800

    change code architecture

--- src/data_loader.py
+++ src/data_loader.py
@@ -91,3 +91,51 @@ class DataLoaderian(data.Dataset):
 		return self.imgs.shape[0]
 
 
+img_h = 1024
+img_w = 2048
+cls = 29
+min_area = 20000
+max_area = 1024*2048/4
+min_total_missing_area = 2000
+
+def add_mask_onehot(file, file_index):
+    with open(file, 'r') as f:
+        text = json.load(f)
+    mask = np.ones((img_h,img_w))
+    objects_list = text['objects']
+    # sort object according to their idx
+    objects_list = sorted(objects_list, key=lambda k: k['idx'])
+    num_obj = len(objects_list)
+    while True:
+        rand_idx = randint(0, num_obj-1)
+        # check whether area fullfill requirement
+        x_1, x_2, y_1, y_2 = objects_list[rand_idx]['x_min'], objects_list[rand_idx]['x_max'], objects_list[rand_idx]['y_min'], objects_list[rand_idx]['y_max']
+        bbox_area = (x_2-x_1)*(y_2-y_1)
+        if bbox_area > max_area or bbox_area < min_area: 
+            continue
+        # generate random crop
+#         print(rand_idx)
+        w = x_2-x_1
+        h = y_2-y_1
+#         print(x_1,x_2,y_1,y_2)
+        rand_w = randint(w//2, w)
+        rand_h = randint(h//2, h)
+        rand_x1 = randint(x_1, x_2-rand_w)
+        rand_y1 = randint(y_1, y_2-rand_h)
+        rand_x2 = rand_x1 + rand_w
+        rand_y2 = rand_y1 + rand_h
+        mask[rand_y1:rand_y2, rand_x1:rand_x2] = 0
+#         print("axis",rand_x1,rand_x2,rand_y1,rand_y2)
+        break
+
+    one_hot = np.zeros(cls)
+    # generate one-hot vector
+    for idx in objects_list[rand_idx]['intersection_id']:
+        # check whether its bbox is fully contained in cropped window
+        # check whether its area is bigger than the threshold
+        x_1,x_2,y_1,y_2 = objects_list[idx]['x_min'],objects_list[idx]['x_max'],objects_list[idx]['y_min'],objects_list[idx]['y_max']
+        if x_1>rand_x1 and x_2<rand_x2 and y_1>rand_y1 and y_2<rand_y2 and (x_2-x_1)*(y_2-y_1)>min_total_missing_area:
+            lin_idx = label2index[objects_list[idx]['label']] # label that lin set
+            one_hot[lin_idx] = one_hot[lin_idx] + 1 
+        
+    return mask.astype(bool), one_hot.astype(np.int8)
--- src/data_utils.py
+++ src/data_utils.py
@@ -22,10 +22,10 @@ from cityscape_utils import *
 		val_segs	: (n_v, h, w)
 		val_masks	: (n_v, h, w)
 '''
-def get_data(dataset, flip=True, val=True):
+def get_data(dataset, epoch=0, flip=True, val=True):
 	### explicitly set flip = True #######
 	if dataset == "cityscape":
-		dataset_train_dir = "Dataset/Cityscape/train_data"
+		dataset_train_dir = "Dataset/Cityscape_level1/train/"
 		dataset_val_dir = "Dataset/Cityscape/val_data"
 
 		seg_dir = "Dataset/Cityscape/gtFine"
@@ -33,69 +33,12 @@ def get_data(dataset, flip=True, val=True):
 		if not val:
 			if os.path.exists(dataset_train_dir+"_segs.npy"): # if stored file exist
 				sys.stdout.write("loading cityscape train dataset from {} ".format(dataset_train_dir))
-				train_segs = np.load(dataset_train_dir+"_segs.npy")
-				train_imgs = np.load(dataset_train_dir+"_imgs.npy")
-				train_masks = np.load(dataset_train_dir+"_masks.npy")
+				train_masks = np.load(dataset_train_dir+"mask_one_hot/epoch"+str(epoch)+"_mask.npy")
+				train_onehots = np.load(dataset_train_dir+"mask_one_hot/epoch"+str(epoch)+"_onehot.npy")
+				train_imgs = None	# for training segmentation network, we don't need this for now
+				train_segs = np.load(dataset_train_dir+"_seg.npy")	# ground-truth label
 				print("loaded")
 				train_size = train_segs.shape[0]
-			else:  # else , loading one time, which cost 1 hour
-				train_seg_dir = os.path.join(seg_dir, 'train')
-				train_seg_files = glob.glob( train_seg_dir+ '/**/*labelIds.png', recursive=True)
-				train_segs = []
-				train_imgs = []
-				train_masks = []
-				train_size = len(train_seg_files)
-
-				for o,train_seg_file in enumerate(train_seg_files):
-					train_img_file = train_seg_file.replace("gtFine_labelIds", "leftImg8bit")
-					train_img_file = train_img_file.replace("gtFine", "leftImg8bit")
-					train_mask_file = train_seg_file.replace("labelIds.png", "polygons_mask.json")
-
-					# tic = time.time()
-					seg = np.vectorize(seg_id2index.get)(imread(train_seg_file))
-					assert seg.shape == (1024, 2048), seg.shape
-					img = imread(train_img_file, mode="RGB")
-					assert img.shape == (1024, 2048, 3), img.shape
-					# toc = time.time() 
-					# img_load_time = toc - tic
-					with open(train_mask_file, 'r') as f:
-						file = json.load(f)
-						mask = np.array(file['mask'])
-					assert mask.shape == (1024, 2048), mask.shape
-					# mask_load_time = time.time()-toc
-					train_segs.append(seg.astype(np.uint8))
-					train_imgs.append(img)
-					train_masks.append(mask.astype(np.uint8))
-					sys.stdout.write("\rloading cityscape train dataset <<<<<<< {}/{} >>>>>> ".format(o+1, train_size))
-					sys.stdout.flush()
-				print()
-
-				if flip:
-					for i in range(train_size):
-						train_flip_seg  = np.flip(train_segs[i],1)
-						train_flip_mask = np.flip(train_masks[i],1)
-						train_flip_img  = np.flip(train_imgs[i],1)
-						train_segs.append(train_flip_seg)
-						train_imgs.append(train_flip_img)
-						train_masks.append(train_flip_mask)
-						sys.stdout.write("\rflipping cityscape train dataset <<<<<<< {}/{} >>>>>>".format(i+1, train_size))
-						sys.stdout.flush()
-					print()
-				train_segs 	= np.array(train_segs)
-				train_imgs 	= np.array(train_imgs)
-				train_masks = np.array(train_masks)
-
-
-				print("shuffling")
-				sys.stdout.flush()
-				np.random.shuffle(train_segs)
-				np.random.shuffle(train_imgs)
-				np.random.shuffle(train_masks)
-
-				print("saving train data into {}".format(dataset_train_dir))		
-				np.save(dataset_train_dir+"_segs",train_segs)
-				np.save(dataset_train_dir+"_imgs",train_imgs)
-				np.save(dataset_train_dir+"_masks",train_masks)
 
 			return n_classes, train_imgs, train_segs, train_masks, None, None ,None
 
